5537e1df290e3710c7135004af5291479112e69a
Blastor99 <miguel_posada25@outlook.com>
2025-01-14 17:17:26 -0600

    Create Blastor99.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Blastor99.py
@@ -0,0 +1,11 @@
+#https://python.org
+
+#comentario en una sola línea
+
+""" 
+Comentario en varias líneas
+de texto. (No afecta el codigo, solo es comentatio para 
+documentación o aclaraciones)
+"""
+
+my_variable= "Variable 1"
